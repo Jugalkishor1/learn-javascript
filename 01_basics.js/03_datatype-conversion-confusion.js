e457272f8edf94bd9bca300e048e70f9a81f7341
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-06-22 22:12:09 +0530

    Clear some confusion of datatype conversion

---
+++ 01_basics.js/03_datatype-conversion-confusion.js
@@ -0,0 +1,24 @@
+let score = "75"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33 it will easily convert to number
+// "33abc" => NaN
+// true => 1: false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 2 => false
+// "" => false; "jugal" => true
