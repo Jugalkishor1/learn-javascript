a2429b473538c07b6d7821f681b8a4f131e09260
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-06-22 21:44:32 +0530

    Learn about datatypes

---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS as newer version.
+
+// console.log(3 + 3);  we are using nodejs, not browser
+
+let name = "Jugal"
+let age = 18
+let isLoggedIn = true
+
+// Datatypes:
+
+// Number => 2 to power 53 (range)
+// Bigint = number is very big (Its specially used in stock market trading)
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined
+// symbol => mostly for uniqe
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
