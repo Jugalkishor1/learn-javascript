66e4828bebfa95fdce724514f7e7ab4428671256
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-06-29 21:40:05 +0530

    Learn about comparison in js

---
+++ 01_basics.js/04_comparison.js
@@ -0,0 +1,30 @@
+
+// Not any problem with this type of comparison because both values have same datatype
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);   // Avoid this bcoz here we are comparing different datatypes values
+// console.log("9" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+                            // Avoid this type of comparisons
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+
+// == and ===
+
+console.log("2" == 2);    // true
+console.log("2" === 2);   // false
+
+// ==  check only values but === also check datatypes
