5a5bc6bfbdc20ec8193a500f74efac95b880f1bd
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-06-29 21:20:10 +0530

    Operators and converstions

--- 01_basics.js/03_datatype-conversion-confusion.js
+++ 01_basics.js/03_datatype-conversion-confusion.js
@@ -22,3 +22,35 @@ console.log(booleanIsLoggedIn);
 
 // 1 => true; 2 => false
 // "" => false; "jugal" => true
+
+
+
+
+
+// ***************** Operations ******************************
+
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);  // Prefix and postfix js Read more on: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
