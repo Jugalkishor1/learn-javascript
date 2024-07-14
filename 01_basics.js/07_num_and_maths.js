8cb465de5bf47d7b2a6e6c51a3f6e6b8d3ffb938
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-07-14 15:41:42 +0530

    Learn about number in js

---
+++ 01_basics.js/07_num_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);   // U can see all properties of this by console.log in browsers console
+
+// console.log(balance.toString().length);
+
+const recharge = 123.442
+// console.log(recharge.toFixed(2));
+
+const newRecharge = 123.8421
+
+console.log(newRecharge.toPrecision(4));
+
+const hundreds = 1056322
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+// Try different properties of Number in console by : Number.
+// Number.MAX_VALUE/MIN_VALUE etc
+
+
+
+// =====================  Maths ==============================================
+
+
